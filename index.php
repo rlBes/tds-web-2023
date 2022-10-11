a50d760cf380795668551a5a8016728e6d01f812
rlBes <rl.besnouin@sts-sio-caen.info>
2022-10-11 11:55:43 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,41 @@
 <?php
 $message = $_GET['message']??"Bonjour";
 $qui=$_GET['qui']??'Tout le monde !';
+$color=$_GET['Color']??'black';
+$color=$_GET['bgColor']??'#ffffff';
+$fontsize=$_GET['fontsize']??'14px';
+extract($_GET);
 ?>
+
 
 <ul> 
     <li><a href="?message=Hi&qui=world">En anglais</a></li>
     <li><a href="?message=Salut&qui=à tous">En Français</a></li>
     <li><a href="?">Par défaut</a></li>
 </ul>
-<form method="get" action="hello.php">
-    <input type="text" name="message" placeholder="saisir un message">
-    <input type="text" name="qui" placeholder="saisir à qui ?">
+
+
+<form method="get" action="index.php">
+    <fieldset>
+        <legend>Message :</legend>
+        <input type="text" name="message" value="<?=$message?>" placeholder="saisir un message">
+        <input type="text" name="qui" value="<?=$qui?>" placeholder="saisir à qui ?">
+        <button type="submit">Valider</button>
+    </fieldset>
+    <fieldset>
+        <legend>Styles :</legend>
+            <input type="color" name="color" value="<?=$color?>">
+            <input type="color" name="bgColor" value="<?=$bgColor?>">
+            <input type="number" name="fontsize" min="5" max="120" value="<?=$fontsize?>">
+    </fielset>
     <button type="submit">Valider</button>
 </form>
 
 
 <h1><?=$message?></h1>
-<p><?=$qui?></p>
+<p style="color : <?=$color?>;background-color:<?=$bgColor?>;font-size: <?=$fontsize?>px">
+<?=$qui?>
+</p>
 
 
 
