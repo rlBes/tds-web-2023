318832eb907ece8822c91ceafd382606970929e8
rlBes <rl.besnouin@sts-sio-caen.info>
2022-12-13 13:40:01 +0100

    f

---
+++ td0/Site projet 1/pages/a-propos.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+  <head>
+            <title>site présentation</title>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width,
+            initial-scale=1">
+            <link rel="stylesheet" href="css/projet.css">
+            <link rel="stylesheet" href="css/bg.css">
+                       
+  </head>
+    <body>
+
+    <main>
+      <div id="mySidebar" class="sidebar">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a>
+        <a href="accueil.php">Accueil</a>
+        <a href="a-propos.php">A propos</a>
+        <a href="compétences.php">Compétences</a>
+        <a href="éxpérience.php">Expériences</a>
+        <a href="formation.php">Formation</a>
+        <a href="contact.php">Contactez-nous</a>
+      </div>
+    
+    <div id="main">
+      <button class="openbtn" onclick="openNav()">☰</button>  
+    </div>
+    
+    <script>
+    function openNav() {
+      document.getElementById("mySidebar").style.width = "250px";
+      document.getElementById("main").style.marginLeft = "250px";
+    }
+    
+    function closeNav() {
+      document.getElementById("mySidebar").style.width = "0";
+      document.getElementById("main").style.marginLeft= "0";
+    }
+    </script>
+    </header>
+
+    <main>
+
+    <?php
+      $data = yaml_parse_file('DATA/a-propos.yaml');
+      echo '<p>'.$data['presentation'].'</p>';
+      echo '<br>';
+      echo '<p>'.$data['accroche'].'</p>';
+      echo  '<img id="moi" src="'.$data['image'].'">'
+    ?>
+    </main>   
+</body>
---
+++ td0/Site projet 1/pages/contact.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+  <head>
+            <title>site présentation</title>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width,
+            initial-scale=1">
+            <link rel="stylesheet" href="css/projet.css">
+            <link rel="stylesheet" href="css/bg.css">
+                       
+  </head>
+    <body>
+
+    <main>
+      <div id="mySidebar" class="sidebar">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a>
+        <a href="accueil.php">Accueil</a>
+        <a href="a-propos.php">A propos</a>
+        <a href="compétences.php">Compétences</a>
+        <a href="éxpérience.php">Expériences</a>
+        <a href="formation.php">Formation</a>
+        <a href="contact.php">Contactez-nous</a>
+      </div>
+    
+    <div id="main">
+      <button class="openbtn" onclick="openNav()">☰</button>  
+    </div>
+    
+    <script>
+    function openNav() {
+      document.getElementById("mySidebar").style.width = "250px";
+      document.getElementById("main").style.marginLeft = "250px";
+    }
+    
+    function closeNav() {
+      document.getElementById("mySidebar").style.width = "0";
+      document.getElementById("main").style.marginLeft= "0";
+    }
+    </script>
+      
+</header>
+
+<main>
+
+<div class="contact">
+  <form action="send.php" method="post">
+
+    <label for="fname">Nom</label>
+    <input type="text" id="fname" name="firstname" placeholder="Jean Neymar..">
+
+    <label for="email">E-Mail</label>
+    <input type="text" id="email" name="email" placeholder="JN@truc.fr..">
+
+    <label for="object">Objet du message</label>
+    <input type="text" id="object" name="object" placeholder="Je conseil de changer..">
+
+    <label for="message">Message</label>
+    <textarea id="message" name="message" placeholder="Je trouve ton site très beau.." style="height:200px"></textarea>
+
+    <input type="submit">
+
+</form>
+
+</main>
+
+
+</body>
